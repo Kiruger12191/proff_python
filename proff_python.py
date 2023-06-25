50719d2185ee7787af2a98667f4f375009cf27ed
Kiruger12191 <125084062+Kiruger12191@users.noreply.github.com>
2023-06-25 22:27:15 +0300

    Update proff_python.py

--- proff_python.py
+++ proff_python.py
@@ -1,71 +1,42 @@
 import re
 import csv
 
-with open("phonebook_raw.csv", 'r', encoding='utf-8') as f:
-    rows = csv.reader(f, delimiter=",")
-    contacts_list = list(rows)
-    new_list = []
-
-
-def names_moving():
-    name_pattern = r'([А-Я])'
-    name_substitution = r' \1'
-    for column in contacts_list[1:]:
-        line = column[0] + column[1] + column[2]
-        if len((re.sub(name_pattern, name_substitution, line).split())) == 3:
-            column[0] = re.sub(name_pattern, name_substitution, line).split()[0]
-            column[1] = re.sub(name_pattern, name_substitution, line).split()[1]
-            column[2] = re.sub(name_pattern, name_substitution, line).split()[2]
-        elif len((re.sub(name_pattern, name_substitution, line).split())) == 2:
-            column[0] = re.sub(name_pattern, name_substitution, line).split()[0]
-            column[1] = re.sub(name_pattern, name_substitution, line).split()[1]
-            column[2] = ''
-        elif len((re.sub(name_pattern, name_substitution, line).split())) == 1:
-            column[0] = re.sub(name_pattern, name_substitution, line).split()[0]
-            column[1] = ''
-            column[2] = ''
-    return
-
-
-def phone_number_formatting():
-    phone_pattern = re.compile(
-        r'(\+7|8)?\s*\(?(\d{3})\)?\s*\D?(\d{3})[-\s+]?(\d{2})-?(\d{2})((\s)?\(?(доб.)?\s?(\d+)\)?)?')
-    phone_substitution = r'+7(\2)\3-\4-\5\7\8\9'
-    for column in contacts_list:
-        column[5] = phone_pattern.sub(phone_substitution, column[5])
-    return
-
-
-def duplicates_combining():
-    for column in contacts_list[1:]:
-        first_name = column[0]
-        last_name = column[1]
-        for contact in contacts_list:
-            new_first_name = contact[0]
-            new_last_name = contact[1]
-            if first_name == new_first_name and last_name == new_last_name:
-                if column[2] == '':
-                    column[2] = contact[2]
-                if column[3] == '':
-                    column[3] = contact[3]
-                if column[4] == '':
-                    column[4] = contact[4]
-                if column[5] == '':
-                    column[5] = contact[5]
-                if column[6] == '':
-                    column[6] = contact[6]
-
-    for contact in contacts_list:
-        if contact not in new_list:
-            new_list.append(contact)
-    return new_list
-
-
 if __name__ == '__main__':
-    names_moving()
-    phone_number_formatting()
-    duplicates_combining()
+    with open("phonebook_raw.csv", encoding='utf-8') as f:
+        rows = csv.reader(f, delimiter=",")
+        contacts_list = list(rows)
 
+    phonebook = [['lastname', 'firstname', 'surname', 'organization',
+                  'position', 'phone', 'email']]
+    book_raw = {}
+    for row in contacts_list[1:]:
+        name_row = ' '.join(row[0:3]).split()
+        lastname, firstname, surname = '', '', ''
+        if len(name_row) == 3:
+            lastname, firstname, surname = name_row
+        else:
+            name_row_upd = ['' for i in range(3 - len(name_row))]
+            lastname, firstname, surname = name_row + name_row_upd
+        organization, position, phone, email = row[3:7]
+        pat1 = r'(\+7|8)?\s*\(?(\d{3})\)?\D*?(\d{3})\D*?(\d{2})'r'\D*?(\d{2})'
+        pat2 = r'(\s*\(?[доб\.]*\s*(\d+)\)?)?'
+        pattern = pat1 + pat2
+        if 'доб' in phone:
+            sub_pattern = r'+7(\2)\3-\4-\5 доб. \7'
+            phone = re.sub(pattern, sub_pattern, phone)
+        else:
+            sub_pattern = r'+7(\2)\3-\4-\5'
+            phone = re.sub(pattern, sub_pattern, phone)
+
+        values = [firstname, surname, organization, position, phone, email]
+        if lastname not in book_raw:
+            book_raw[lastname] = values
+        else:
+            for i in range(len(book_raw[lastname])):
+                if book_raw[lastname][i] == '':
+                    book_raw[lastname][i] = values[i]
+    for i in book_raw:
+        phonebook.append([i] + book_raw[i])
     with open("phonebook.csv", "w", encoding='utf-8') as f:
         datawriter = csv.writer(f, delimiter=',')
-        datawriter.writerows(new_list)
+        datawriter.writerows(phonebook)
